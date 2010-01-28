c8cda34afc34d5b2abfb18083889eeb7816e4fbb
vti <viacheslav.t@gmail.com>
2010-01-28 23:36:34 +0100

    Added cache and exception handling

--- lib/MojoX/Renderer/Haml.pm
+++ lib/MojoX/Renderer/Haml.pm
@@ -6,77 +6,83 @@ use strict;
 use base 'Mojo::Base';
 
 use Text::Haml;
-use Mojo::ByteStream;
+use Mojo::ByteStream 'b';
 
 our $VERSION = '0.010101';
 
-__PACKAGE__->attr('haml');
-
 sub build {
     my $self = shift->SUPER::new(@_);
-    $self->_init(@_);
+
     return sub { $self->_render(@_) }
 }
 
-sub _init {
-    my $self = shift;
-    my %args = @_;
-
-    my $mojo = delete $args{mojo};
-
-    my %config = (%{$args{haml_options} || {}});
-
-    $config{escape} = <<'EOF';
+my $ESCAPE = <<'EOF';
     my $v = shift;
     ref $v && ref $v eq 'Mojo::ByteStream'
       ? "$v"
       : Mojo::ByteStream->new($v)->xml_escape->to_string;
 EOF
 
-    $self->haml(Text::Haml->new(%config));
-
-    return $self;
-}
-
 sub _render {
     my ($self, $r, $c, $output, $options) = @_;
-
-    use Data::Dumper;
 
     my $path;
     unless ($path = $c->stash->{'template_path'}) {
         $path = $r->template_path($options);
     }
 
+    my $list = join ', ', sort keys %{$c->stash};
+    my $cache = b("$path($list)")->md5_sum->to_string;
+    $r->{_haml_cache} ||= {};
+
     my $t = $r->template_name($options);
 
-    if ($r->can('helper')) {
-        $self->haml->helpers_arg($c);
-        $self->haml->helpers($r->helper);
+    my $haml = $r->{_haml_cache}->{$cache};
+
+    # Interpret again
+    if ($haml && $haml->compiled) {
+        $$output = $haml->interpret(%{$c->stash});
     }
 
-    $c->app->log->debug("Rendering $path");
-
-    # Try template
-    if (-r $path) { $$output = $self->haml->render_file($path, %{$c->stash}) }
-
-    # Try DATA section
-    elsif (my $d = $r->get_inline_template($c, $t)) {
-        $$output = $self->haml->render($d, %{$c->stash});
-    }
-
-    # No template
+    # No cache
     else {
-        $c->app->log->error(qq/Template "$t" missing or not readable./);
-        $c->render_not_found;
-        return;
+        $haml ||= Text::Haml->new(escape => $ESCAPE);
+
+        if ($r->can('helper')) {
+            $haml->helpers_arg($c);
+            $haml->helpers($r->helper);
+        }
+
+        $c->app->log->debug("Rendering $path");
+
+        # Try template
+        if (-r $path) {
+            $$output = $haml->render_file($path, %{$c->stash});
+        }
+
+        # Try DATA section
+        elsif (my $d = $r->get_inline_template($c, $t)) {
+            $$output = $haml->render($d, %{$c->stash});
+        }
+
+        # No template
+        else {
+            $c->app->log->error(qq/Template "$t" missing or not readable./);
+            $c->render_not_found;
+            return;
+        }
     }
 
     unless (defined $$output) {
-        $$output = $self->haml->error;
-        die $self->haml->error;
+        my $e = $$output;
+        $$output = '';
+        $c->app->log->error( qq/Template error in "$t": / . $haml->error);
+        $c->render_exception($haml->error);
+
         return 0;
     }
+
+    $r->{_haml_cache}->{$cache} ||= $haml;
 
     return 1;
 }
--- t/plugin.t
+++ t/plugin.t
@@ -3,22 +3,20 @@
 use strict;
 use warnings;
 
-use Test::More tests => 3;
+use Test::More tests => 9;
 
 use Test::Mojo;
 use Mojo::Client;
 use Mojolicious::Lite;
 
 # Silence
-app->log->level('error');
+app->log->level('fatal');
 
 plugin 'haml_renderer';
 
-get '/' => sub {
-    my $self = shift;
+get '/' => 'root';
 
-    $self->render;
-} => 'root';
+get '/error' => 'error';
 
 my $client = Mojo::Client->new(app => app);
 app->client($client);
@@ -26,10 +24,27 @@ app->client($client);
 my $t = Test::Mojo->new;
 $t->client($client);
 
+# No cache
 $t->get_ok('/')->status_is(200)->content_is("<foo></foo>\n");
+
+# Cache hit
+$t->get_ok('/')->status_is(200)->content_is("<foo></foo>\n");
+
+# Error
+$t->get_ok('/error')->status_is(500)->content_like(qr/^Exception:\nsyntax error/);
 
 1;
 __DATA__
 
 @@ root.html.haml
 %foo
+
+@@ error.html.haml
+= 1 + {
+
+@@ exception.html.haml
+Exception:
+= exception
+
+@@ not_found.html.haml
+Not found
