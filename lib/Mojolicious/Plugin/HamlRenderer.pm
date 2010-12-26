7afc59bd934289f352e50c5f97240ea57c38c9c7
Marcus Ramberg <marcus@nordaaker.com>
2010-12-26 01:08:59 +0100

    Add simple documentation stub

--- lib/Mojolicious/Plugin/HamlRenderer.pm
+++ lib/Mojolicious/Plugin/HamlRenderer.pm
@@ -19,3 +19,33 @@ sub register {
 }
 
 1;
+
+=head2 NAME
+
+Mojolicious::Plugin::HamlRenderer - Load HAML renderer
+
+=head2 SYNOPSIS
+
+# lite app
+plugin 'haml_renderer';
+# or normal app
+$self->plugin 'haml_renderer';
+
+=head2 DESCRIPTION
+
+Simple plugin to load HAML renderer into your Mojolicious app.
+
+=head1 AUTHOR
+
+Viacheslav Tykhanovskyi, C<viacheslav.t@gmail.com>.
+
+=head1 COPYRIGHT
+
+Copyright (C) 2008-2009, Viacheslav Tykhanovskyi.
+
+This program is free software, you can redistribute it and/or modify it under
+the same terms as Perl 5.10.
+
+=cut
+
+
